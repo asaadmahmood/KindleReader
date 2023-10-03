1f09b44eaee4f15edc270327f66d988ce3c9a6e3
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2023-10-03 19:38:45 +0500

    Initial Commit

---
+++ KindleReader/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KindleReader
+//
+//  Created by Asaad Mahmood on 03/10/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
